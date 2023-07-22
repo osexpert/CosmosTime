87802bcbac8eb3363294805c7e14c981c7822905
osexpert <33638596+osexpert@users.noreply.github.com>
2023-07-23 00:37:09 +0200

    0.0.2

--- nuget push.bat
+++ nuget push.bat
@@ -1,5 +1,5 @@
 REM set myKey=
-set ver=0.0.1
+set ver=0.0.2
 
 nuget push bin\Packages\Release\NuGet\CosmosTime.%ver%.nupkg -src https://api.nuget.org/v3/index.json -ApiKey %myKey%
 nuget push bin\Packages\Release\NuGet\CosmosTime.Serialization.Dapper.%ver%.nupkg -src https://api.nuget.org/v3/index.json -ApiKey %myKey%
