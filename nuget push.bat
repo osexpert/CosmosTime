66fadd5ef49e87b301b83c95377bd9b2580b80bd
osexpert <33638596+osexpert@users.noreply.github.com>
2023-08-11 13:31:35 +0200

    require set envars

--- nuget push.bat
+++ nuget push.bat
@@ -1,7 +1,7 @@
-REM set myKey=
-set ver=0.0.2
+REM set key=
+REM set ver=
 
-nuget push bin\Packages\Release\NuGet\CosmosTime.%ver%.nupkg -src https://api.nuget.org/v3/index.json -ApiKey %myKey%
-nuget push bin\Packages\Release\NuGet\CosmosTime.Serialization.Dapper.%ver%.nupkg -src https://api.nuget.org/v3/index.json -ApiKey %myKey%
-nuget push bin\Packages\Release\NuGet\CosmosTime.Serialization.JsonNet.%ver%.nupkg -src https://api.nuget.org/v3/index.json -ApiKey %myKey%
-nuget push bin\Packages\Release\NuGet\CosmosTime.Serialization.SystemTextJson.%ver%.nupkg -src https://api.nuget.org/v3/index.json -ApiKey %myKey%
+nuget push bin\Packages\Release\NuGet\CosmosTime.%ver%.nupkg -src https://api.nuget.org/v3/index.json -ApiKey %key%
+nuget push bin\Packages\Release\NuGet\CosmosTime.Serialization.Dapper.%ver%.nupkg -src https://api.nuget.org/v3/index.json -ApiKey %key%
+nuget push bin\Packages\Release\NuGet\CosmosTime.Serialization.JsonNet.%ver%.nupkg -src https://api.nuget.org/v3/index.json -ApiKey %key%
+nuget push bin\Packages\Release\NuGet\CosmosTime.Serialization.SystemTextJson.%ver%.nupkg -src https://api.nuget.org/v3/index.json -ApiKey %key%
