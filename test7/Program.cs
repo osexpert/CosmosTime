04a611ff80802e6b20e5831e242ae30f4f25d057
osexpert <33638596+osexpert@users.noreply.github.com>
2024-04-05 10:28:18 +0200

    misc

--- test7/Program.cs
+++ test7/Program.cs
@@ -8,15 +8,9 @@ namespace test7
         {
             var poison = TimeZoneInfo.FindSystemTimeZoneById("uTc");
 
-            var list = TimeZoneInfo.GetSystemTimeZones().Where(tz => tz.DisplayName == "(UTC) Coordinated Universal Time" || tz.DisplayName == "UTC").ToList();
-            var tz1 = list.Single(); // tz1.DisplayName: "(UTC) Coordinated Universal Time"
-            var tz2 = TimeZoneInfo.Utc; // tz1.DisplayName: "UTC"
-            var eq = tz1 == tz2; // false
-
             TimeZoneInfo.Utc.HasIanaId();
 
-            var l = TimeZoneInfo.GetSystemTimeZones().Where(tz => tz.DisplayName == "(UTC) Coordinated Universal Time" || tz.DisplayName == "UTC").ToList();
-
+            var l = TimeZoneInfo.GetSystemTimeZones().Where(tz => tz.StandardName == "Coordinated Universal Time" || tz.StandardName == "UTC").ToList();
 
             var tzIsUtc2 = TimeZoneInfo.FindSystemTimeZoneById("UTC") == TimeZoneInfo.Utc; // true
             var tzIsUtc3 = TimeZoneInfo.FindSystemTimeZoneById("Etc/UTC") == TimeZoneInfo.Utc; // false
@@ -35,7 +29,7 @@ namespace test7
 
 
             Console.WriteLine("Hello, World!");
-            var tz = TimeZoneInfo.FindSystemTimeZoneById("Europe/London");
+            var ttz = TimeZoneInfo.FindSystemTimeZoneById("Europe/London");
             var utz = TimeZoneInfo.FindSystemTimeZoneById("UTC");
             var tzz = TimeZoneInfo.CreateCustomTimeZone("lolz", utz.BaseUtcOffset, utz.DisplayName, utz.StandardName, utz.DaylightName, utz.GetAdjustmentRules());
             var tzz2 = TimeZoneInfo.CreateCustomTimeZone("lolz", utz.BaseUtcOffset, utz.DisplayName, utz.StandardName, utz.DaylightName, utz.GetAdjustmentRules());
